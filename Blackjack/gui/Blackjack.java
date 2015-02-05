241b28b808b2c35e30b0ed972f9496c6e86c645d
Austin <hillam.mail@gmail.com>
2015-02-04 21:09:34 -0500

    blackjack gui upload

--- Blackjack/gui/Blackjack.java
+++ Blackjack/gui/Blackjack.java
@@ -1,4 +1,14 @@
 import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.Graphics;
+import java.awt.Color;
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 
 public class Blackjack extends JPanel{
 
@@ -8,6 +18,11 @@ public class Blackjack extends JPanel{
 	boolean winner = false, stay = false;
 
 	Blackjack(){
+		setSize(800,600);
+		addMouseListener(this);
+	}
 
+	public void paintComponent(Graphics g){
+		
 	}
 }
