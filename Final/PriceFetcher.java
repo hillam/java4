d7b9612344918211fbe9ed7ff5340f850a34570f
Austin <hillam.mail@gmail.com>
2015-03-01 20:06:30 -0500

    Final - getPrice() mk 1

--- Final/PriceFetcher.java
+++ Final/PriceFetcher.java
@@ -1,8 +1,12 @@
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+
+	/*--------------------------------------------------------------------------
+		TODO: CURRENTLY NOT WORKING FOR FREE TO PLAY OR AGECHECK 
+	--------------------------------------------------------------------------*/
 
 public class PriceFetcher{
 
@@ -10,27 +14,39 @@ public class PriceFetcher{
 
 	}
 
-	public static String getPrice(String url){
-		String content = null;
-		URLConnection connection = null;
-		try {
-			connection =  new URL(url).openConnection();
-			Scanner scanner = new Scanner(connection.getInputStream());
-			scanner.useDelimiter("\\Z");
-			content = scanner.next();
-		}catch ( Exception ex ) {
-		    ex.printStackTrace();
+	public static String getPrice(String url) throws Exception{
+		String content = "";
+		url = url;
+
+		URL u = new URL(url);
+
+		HttpURLConnection connection = (HttpURLConnection) u.openConnection();
+
+		Scanner scanner = new Scanner(connection.getInputStream());
+		while(scanner.hasNextLine()){
+			content += scanner.nextLine();
 		}
-		System.out.println(content);
+		scanner.close();
 
-		//regex: <div class\=\"game_purchase_action_bg\">\s+<div class\=\"game_purchase_price price\">\s+&#36;(\d\d\.\d\d)\s+<\/div>
-		Pattern p = Pattern.compile("<div class=\"game_purchase_action_bg\">[\\s\\n]+<div class=\"game_purchase_price price\">[\\s\\n]+&#36;(\\d\\d\\.\\d\\d)[\\s\\n]+</div>");
-		Matcher m = p.matcher(content);
+		//System.out.println(content);
 
-		return m.group(1);
+		Pattern p1 = Pattern.compile("<div class=\"discount_final_price\">&#36;([\\d\\.]+)");
+		Pattern p2 = Pattern.compile("<div class=\"game_purchase_price price\">[^&]+&#36;([\\d\\.]+)");
+		
+		Matcher m1 = p1.matcher(content);
+		Matcher m2 = p2.matcher(content);
+
+		//System.out.println(m1.find() + " " + m2.find());
+
+		if(m1.find())
+			return m1.group(1);
+		if (m2.find())
+			return m2.group(1);
+		return "No match found.";
 	}
 
-	public static void main(String[] args){
-		System.out.println(PriceFetcher.getPrice("http://store.steampowered.com/app/322330/"));
+	public static void main(String[] args) throws Exception{
+		System.out.println(PriceFetcher.getPrice("http://store.steampowered.com/app/202200/"));
+		System.out.println(PriceFetcher.getPrice("http://store.steampowered.com/app/226860/"));
 	}
 }
