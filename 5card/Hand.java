d1a460d803b9b114d5443d13f63a39027d53164d
Austin <hillam.mail@gmail.com>
2015-03-05 01:06:18 -0500

    group proj - finished hand ranking (not tested)

--- 5card/Hand.java
+++ 5card/Hand.java
@@ -1,6 +1,7 @@
 import java.awt.Graphics;
 import javax.swing.JPanel;
 import java.lang.Math;
+import java.util.Arrays;
 
 public class Hand{
 	
@@ -85,75 +86,51 @@ public class Hand{
 	public int getScore(){
 		int score = highCard();
 
-		/*if(size > 0){
-			int[] temps = {pair(),flush(),run(),runningFlush(),triple()};
+		int[] temps = {pair(),twoPair(),three(),straight(),flush(),
+						fullHouse(),four(),straight() + flush()};
 
-			for(int i : temps)
-				if(i > score)
-					score = i;
-		)*/
+		for(int i : temps)
+			if(i > score)
+				score = i;
 
 		return score;
 	}
 
-	private int triple(){
+	private int four(){
 		int score = 0;
-		boolean triple = true;
 
-		if(size > 0){
-			Card c = cards[0];
-			for(int i=1;i<size;i++)
-				if(c.getValue() != cards[i].getValue())
-					triple = false;
+		int[] values = new int[5];
+		int[] matches = new int[5];
+		int index = 0;
+		for(int i=0;i<size && !Arrays.asList(matches).contains(3);i++){
+			if(!Arrays.asList(values).contains(cards[i].getValue())){
+				values[index] = cards[i].getValue();
+				matches[index] = 0;
+				index++;				
+			}
+			else
+				matches[Arrays.asList(values).indexOf(cards[i].getValue())]++;
 		}
 
-		if(triple)
-			score = 500 + highCard();
+		index = Arrays.asList(matches).indexOf(3);
+		if(index > -1){
+			score = 700 + values[index];
+		}
 
 		return score;
 	}
 
-	private int runningFlush(){
+	//done
+	private int fullHouse(){
 		int score = 0;
-		boolean rf = true;
 
-		if(size > 0){
-			Card c = cards[0];
-			for(int i=1;i<size;i++)
-				if(c.getSuit() != cards[i].getSuit())
-					rf = false;
-		}
-
-		if(size == 3){ // it should... but just incase
-			int first_diff = Math.abs(cards[0].getValue()-cards[1].getValue());
-			int second_diff = Math.abs(cards[1].getValue()-cards[2].getValue());
-			if(!(first_diff == 1 && second_diff == 1))
-				rf = false;
-		}
-
-		if(rf)
-			score = 400 + highCard();
+		if(three() > 300 && pair() > 100)
+			score = 600 + highCard();
 
 		return score;
 	}
 
-	private int run(){
-		int score = 0;
-		boolean run = true;
-
-		if(size == 3){ // it should... but just incase
-			int first_diff = Math.abs(cards[0].getValue()-cards[1].getValue());
-			int second_diff = Math.abs(cards[1].getValue()-cards[2].getValue());
-			if(!(first_diff == 1 && second_diff == 1))
-				run = false;
-		}
-
-		if(run)
-			score = 300 + highCard();
-
-		return score;
-	}
-
+	//done
 	private int flush(){
 		int score = 0;
 		boolean flush = true;
@@ -166,32 +143,101 @@ public class Hand{
 		}
 
 		if(flush)
-			score = 200 + highCard();
+			score = 500 + highCard();
 
 		return score;
 	}
 
+	//done
+	private int straight(){
+		int score = 0;
+		boolean straight = true; //start assuming true, try to prove it wrong
+
+		if(size > 0) //it should be, but just incase
+			for(int i=0;i<size;i++)
+				if(sorted[i].getValue() != sorted[0].getValue() + i)
+					straight = false;
+
+		if(straight)
+			score = 400 + highCard();
+
+		return score;
+	}
+
+	//done
+	private int three(){
+		int score = 0;
+
+		int[] values = new int[5];
+		int[] matches = new int[5];
+		int index = 0;
+		for(int i=0;i<size && !Arrays.asList(matches).contains(2);i++){
+			if(!Arrays.asList(values).contains(cards[i].getValue())){
+				values[index] = cards[i].getValue();
+				matches[index] = 0;
+				index++;				
+			}
+			else
+				matches[Arrays.asList(values).indexOf(cards[i].getValue())]++;
+		}
+
+		index = Arrays.asList(matches).indexOf(2);
+		if(index > -1){
+			score = 300 + values[index];
+		}
+
+		return score;
+	}
+
+	//done
 	private int twoPair(){
 		int score = 0;
 
 		int[] values = new int[5];
 		boolean found = false;
+		int index = 0;
 		int match = -1;
 		// loop through cards while !found
 		// if values doesn't contain cards[i].getValue() AND match != cards[i].getValue(), add it and set match = cards[i].getValue()
 		// else if match != -1, exit loop and score = 200 + match OR cards[i].getValue() (whichever is larger)
+		for(int i=0;i<size && !found;i++){
+			if(!Arrays.asList(values).contains(cards[i].getValue()) &&
+					match != cards[i].getValue()){
+				values[index] = cards[i].getValue();
+				match = cards[i].getValue();
+				index++;
+			}
+			else if(match != -1){
+				found = true;
+				score = 200;
+				score += (match > cards[i].getValue() ? match : cards[i].getValue());
+			}
+		}
 
 		return score;
 	}
 
+	//done
 	private int pair(){
 		int score = 0;
 
 		int[] values = new int[5];
 		boolean found = false;
+		int index = 0;
+
 		// loop through cards while !found
 		// if values doesn't contain cards[i].getValue(), add it
 		// else, exit loop and score = 100 + cards[i].getValue()
+		for(int i=0;i<size && !found;i++){
+			if(!Arrays.asList(values).contains(cards[i].getValue())){
+				values[index] = cards[i].getValue();
+				index++;
+			}
+			else{
+				found = true;
+				score = 100 + cards[i].getValue();
+			}
+		}
 
 		return score;
 	}
