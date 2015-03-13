619ca6b0c9ad652408e968c8c4f272afcdc37490
Austin Hill <hillam.mail@gmail.com>
2015-03-13 00:52:14 -0400

    Update PriceFetcher.java

--- Final/PriceFetcher.java
+++ Final/PriceFetcher.java
@@ -5,7 +5,8 @@ import java.util.regex.*;
 
 
 	/*--------------------------------------------------------------------------
-		 
+		 Get and store name and price of a game given it's store url
+		 using steam web api
 	--------------------------------------------------------------------------*/
 
 public class PriceFetcher{
